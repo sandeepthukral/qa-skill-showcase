7f777c256e8eb9bc827a1f1f07ead22de6de0dde
Sandeep Thukral <sandeep.thukral@insided.com>
2020-02-27 08:28:19 +0100

    attach screenshots on failure to report

--- src/test/java/sandeep/geophy/qa/utils/Hooks.java
+++ src/test/java/sandeep/geophy/qa/utils/Hooks.java
@@ -3,8 +3,13 @@ package sandeep.geophy.qa.utils;
 import com.codeborne.selenide.Configuration;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
 
 import static com.codeborne.selenide.Selenide.closeWebDriver;
+import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 
 public class Hooks {
     @Before
@@ -13,7 +18,12 @@ public class Hooks {
         Configuration.reportsFolder = "build/surefire-reports";
     }
     @After
-    public void tearDown() {
+    public void tearDown(Scenario scenario) {
+        if (scenario.isFailed()){
+            WebDriver driver = getWebDriver();
+            final byte[] screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
+            scenario.embed(screenshot, "image/png", "Failure Screenshot");
+        }
         closeWebDriver();
     }
 }

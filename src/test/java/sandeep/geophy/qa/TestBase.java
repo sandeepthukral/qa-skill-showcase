305a5fe614d23365c3809412bf5cf40ec49ccd5c
Sandeep Thukral <sandeep.thukral@insided.com>
2020-02-28 20:18:34 +0100

    preparing for remote driver execution

--- src/test/java/sandeep/geophy/qa/TestBase.java
+++ src/test/java/sandeep/geophy/qa/TestBase.java
@@ -9,6 +9,7 @@ import java.util.Properties;
 public class TestBase {
 
     public static Properties prop;
+    protected static final boolean REMOTE_DRIVER = Boolean.parseBoolean(System.getProperty("REMOTE_DRIVER", "false"));
 
     //TODO instantiate an object that reads data from the props file. Not using props.getPRoperty() everywhere
 
@@ -25,7 +26,11 @@ public class TestBase {
 
         Configuration.startMaximized = false;
         Configuration.browser = prop.getProperty("browser", "chrome");
-        Configuration.browserSize = prop.getProperty("browser.size", "1280x1024");;
+        Configuration.browserSize = prop.getProperty("browser.size", "1280x1024");
         Configuration.baseUrl = prop.getProperty("base_url");
+        if (REMOTE_DRIVER) {
+            Configuration.remote = "http://localhost:4444/wd/hub";
+            Configuration.driverManagerEnabled = false;
+        }
     }
 }
--- src/test/java/sandeep/geophy/qa/utils/Hooks.java
+++ src/test/java/sandeep/geophy/qa/utils/Hooks.java
@@ -4,18 +4,30 @@ import com.codeborne.selenide.Configuration;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import io.cucumber.java.Scenario;
+import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
+import sandeep.geophy.qa.TestBase;
 
 import static com.codeborne.selenide.Selenide.closeWebDriver;
 import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 
-public class Hooks {
+public class Hooks extends TestBase {
+
+    @BeforeEach
+    void setUpEach() {
+        if (REMOTE_DRIVER) {
+            Configuration.driverManagerEnabled = false;
+            Configuration.remote = "http://localhost:4444/wd/hub";
+        }
+    }
+
     @Before
     public void setUp() {
         Configuration.startMaximized = false;
         Configuration.reportsFolder = "build/surefire-reports";
+
     }
     @After
     public void tearDown(Scenario scenario) {
